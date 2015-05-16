d863ccabd88d52cac24b3269f1d79ef192b6da24
random-toto@localhost <random-toto>
2015-05-16 21:23:08 +0200

    amélioré fRandom. ajout de multipleRandom

--- fRandom.py
+++ fRandom.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+import random
+import hashlib
+import time 
+
+random.seed(hashlib.sha256(str(time.mktime(time.localtime())).encode()).hexdigest())
+
 def rangeRandom(a=0, b=1):
     ''' renvoie un nombre aléatoire entre a et b
     
@@ -10,9 +16,14 @@ def rangeRandom(a=0, b=1):
     except:
         raise ValueError("rangeRandom : a ou b non réels.")
     assert a < b
-    import random
-    import hashlib
-    import time 
-    random.seed(hashlib.sha256(str(time.mktime(time.localtime())).encode()).hexdigest())
     return a + (b - a)*(random.random())
+
+def multipleRandom(n=1, a=0, b=1):
+    '''
+    '''
+    try:
+        n = int(n)
+    except:
+        raise ValueError("multipleRandom : Pas un int.")
+    return [rangeRandom(a, b) for i in range(n)]
 
